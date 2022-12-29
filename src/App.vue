e706a4c361e44f403a624b83a1c8179be3225da8
mdayubali <ayubh9288@gmail.com>
2022-12-29 11:48:32 +0600

    two way v-model binding added

--- src/App.vue
+++ src/App.vue
@@ -37,6 +37,7 @@ import { ref } from 'vue'
 <!-- -->
 <script setup>
 import { ref } from 'vue';
+import Databinding from './Databinding.vue';
     const courseName = ref('Laravel with vue js')
     const instructor = ref('Md Rasel Ahmed')
     let count = ref(0)
@@ -46,10 +47,10 @@ import { ref } from 'vue';
   <div class="text-center" > 
     <h2 class="text-xl font-semibold py-5">Course Name: {{ courseName   }}</h2>
     <p class="text-2xl">Instructor: {{ instructor }}</p>
-    <button @click="count++" class="text-3xl font-bold inline py-2 px-4 bg-gray-200 shadow">+</button>
-        <h1 class="font-bold text-3xl">Count Number: {{ count }}</h1>
-    <button v-if="count > 0" @click="count--" class="text-3xl font-bold inline py-2 px-4 bg-gray-300 shadow">-</button>
+
   </div>
+  <hr>
+  <Databinding></Databinding>
 </template>
 
 <style scoped>
---
+++ src/Databinding.vue
@@ -0,0 +1,50 @@
+<script setup>
+import {ref,reactive} from 'vue'
+    // non reactive way  
+    const title = 'It comes from Databinding Component' 
+    // ref reactive way with primitive data
+    const CourseInstructor = ref("Eng. Ibrahim")
+    let count = ref(0)
+     // reactive reactive way with non-primitive data
+    let courseDetails = reactive({
+        CourseName: 'Laravel with vue Js',
+        Duration: '72hours',
+        price : 300
+    })
+    courseDetails.CourseName = "Larave & Vue Js"
+</script>
+<template>
+    <div class="container text-center border shadow p-4 mt-8"> 
+        <h1 class="text-xl font-semibold py-3">{{ title }}</h1>
+        <button @click="count++" class="text-3xl font-bold inline py-2 px-4 bg-gray-200 shadow">+</button>
+        <h1 class="font-bold text-3xl">Count Number: {{ count }}</h1>
+        <input v-model="count" type="text" class="border w-1/2 p-3"> <br> 
+        <h2 class="text-2xl text-center">Put Your Number in box</h2>
+    <button v-if="count > 0" @click="count--" class="text-3xl font-bold inline py-2 px-4 bg-gray-300 shadow">-</button>
+        <hr>
+        <h1 class="text-2xl font-semibold py-3">Instructor Name: {{ CourseInstructor }}</h1>
+        <table class="table-auto">
+            <thead>
+                <tr>
+                    <th>Instructor Name</th>
+                    <th>Course Name</th>
+                    <th>Course duration</th>
+                </tr>
+            </thead>
+            <tbody class="bg-white">
+                <tr>
+                    <td>{{ CourseInstructor }}</td>
+                    <td>{{ courseDetails.CourseName }}</td>
+                    <td>{{ courseDetails.Duration }}</td>
+                </tr>
+            </tbody>
+           
+        </table>
+    </div>
+</template>
+<style scoped>
+    .container{
+        width: 850px;
+    }
+
+</style>
